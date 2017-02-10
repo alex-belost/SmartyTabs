f244bbdfdc5256c96670e3693ff1f1975d5cf2be
alexandr.belostockiy <alexandr.belostockiy@brander.ua>
2017-02-10 15:46:26 +0200

    rebuild

--- smarty-tabs.js
+++ smarty-tabs.js
@@ -1,117 +1,112 @@
-export default (() => {
-    function SmartyTabs( initClass, config ) {
-        if( !(this instanceof SmartyTabs) ) return new SmartyTabs( initClass, config );
-        if( initClass ) {
-            const userConfig = config || {};
-            const init = initClass || '';
-            const that = this;
-            
-            // config
-            const $ = {
-                startIndex     : userConfig.startIndex || that._startIndex,
-                container      : userConfig.container
-                    ? `${init}${userConfig.container}`
-                    : `${init}${that._container}`,
-                navContainer   : userConfig.navContainer || that._navContainer,
-                navList        : that._getEl( userConfig.navList
-                    ? `${init} ${userConfig.navList}`
-                    : `${init} ${that._navList}` ),
-                navItems       : that._getEl( userConfig.navItems
-                    ? `${init} ${userConfig.navItems}`
-                    : `${init} ${that._navItems}`, true ),
-                contentList    : that._getEl( userConfig.contentList
-                    ? `${init} ${userConfig.contentList}`
-                    : `${init} ${that._contentList}` ),
-                contentItems   : that._getEl( userConfig.contentItems
-                    ? `${init} ${userConfig.contentItems}`
-                    : `${init} ${that._contentItems}`, true ),
-                activeClass    : {
-                    controller: userConfig.activeController || that._activeController,
-                    content   : userConfig.activeContent || that._activeContent
-                },
-                controllerSlide: userConfig.controllerSlide || that._controllerSlide,
-                maxScreen      : userConfig.maxScreen || that._maxScreen,
-                maxBool        : false
-            };
-            
-            // start init
-            this._startTab( $.navItems, $.contentItems, $.startIndex, $.activeClass );
-            $.maxBool = this._checkWidth( $.maxScreen );
-            
-            // events
-            window.addEventListener( 'resize', () => {
-                $.maxBool = this._checkWidth( $.maxScreen );
-                if( !$.maxBool ) this._controllerCenter( $.navList );
+class SmatryTabs {
+    constructor( init, userConfig ) {
+        this.config = userConfig || {};
+        this.initClass = init;
+        
+        this._startStartTab();
+        
+        // events
+        window.addEventListener( 'resize', () => {
+            if( this._checkWidth() ) this._tabPosition();
+        } );
+        
+        if( SmatryTabs._el( this._navList() ) ) {
+            SmatryTabs._el( this._navList() ).addEventListener( 'click', ( event ) => {
+                event.preventDefault();
+                let target = event.target;
+                
+                while( target.tagName !== 'UL' ) {
+                    if( target.tagName === 'LI' ) break;
+                    target = target.parentNode;
+                }
+                
+                if( !this._checkWidth() ) this._tabPosition( target );
+                
+                this._removeActiveClass();
+                
+                // add for current item active class
+                let targetIndex = this._targetTabIndex( target );
+                
+                if( targetIndex + 1 ) {
+                    SmatryTabs._el( this._contentItems(), true )[ targetIndex ]
+                        .classList.add( this._activeClass().content );
+                    target.classList.add( this._activeClass().nav );
+                }
+                
             } );
-            if( $.navList ) {
-                $.navList.addEventListener( 'click', ( event ) => {
-                    event.preventDefault();
-                    let target = event.target;
-                    
-                    while( target.tagName !== 'UL' ) {
-                        if( target.tagName === 'LI' ) break;
-                        target = target.parentNode;
-                    }
-                    
-                    if( $.maxBool ) this._controllerCenter( $.navList, target );
-                    
-                    // remove active class
-                    this._removeClass( $.navItems, $.activeClass.controller );
-                    this._removeClass( $.contentItems, $.activeClass.content );
-                    
-                    // add for current item active class
-                    let controllerIndex = this._currentTab( target, $.activeClass.controller, $.navList );
-                    if( controllerIndex + 1 ) {
-                        $.contentItems[ controllerIndex ].classList.add( $.activeClass.content );
-                        target.classList.add( $.activeClass.controller );
-                    }
-                    
-                } );
-            }
         }
     }
-
-    // prototype
-    SmartyTabs.prototype = {
-        _startIndex      : 0,
-        _controllerSlide : true,
-        _maxScreen       : 650,
-        _container       : '.smarty-tabs',
-        _navContainer    : '.smarty-controller',
-        _navList         : '.smarty-controller__list',
-        _contentList     : '.smarty-content',
-        _navItems        : '.smarty-controller__item',
-        _contentItems    : '.smarty-content__item',
-        _activeController: 'smarty-controller__item--active',
-        _activeContent   : 'smarty-content__item--active',
-        _getEl( el, col ) {
-            return col ? document.querySelectorAll( el ) : document.querySelector( el );
-        },
-        _controllerCenter( navList, el ) {
-            let trX = el ? (navList.offsetWidth / 2 - el.offsetWidth / 2) - el.offsetLeft : 0;
-            navList.style.transform = `translate(${trX}px)`;
-        },
-        _checkWidth( max ) {
-            return this._controllerSlide && max <= window.innerWidth;
-        },
-        _removeClass( items, activeClass ) {
-            items.forEach( ( item ) => {
-                if( item.classList.contains( activeClass ) ) item.classList.remove( activeClass )
-            } )
-        },
-        _addClass( item, activeClass ) {
-            item.classList.add( activeClass )
-        },
-        _startTab( controllerItems, contentItems, startIndex, activeClass ) {
-            controllerItems[ startIndex ].classList.add( activeClass.controller );
-            contentItems[ startIndex ].classList.add( activeClass.content );
-        },
-        _currentTab( target, activeClass, navList ) {
-            if( !target.classList.contains( activeClass ) ) {
-                let nodeList = Array.prototype.slice.call( navList.children );
-                return nodeList.indexOf( target );
-            }
+    
+    // default congif
+    _startIndex() { return this.config.startIndex || 0 }
+    
+    _tabSlide() { return this.config.tabSlide || true }
+    
+    _maxScreen() { return this.config.tabSlide || 650 }
+    
+    _container() { return this.initClass + ( this.config.container || '.smarty-tabs' ) }
+    
+    _navWrapper() { return `${this.initClass}` + ( this.config.container || '.smarty-controller' ) }
+    
+    _navList() { return `${this.initClass} ` + ( this.config.container || '.smarty-controller__list') }
+    
+    _navItems() { return `${this.initClass} ` + ( this.config.container || '.smarty-controller__item') }
+    
+    _contentWrapper() { return `${this.initClass} ` + ( this.config.container || '.smarty-content') }
+    
+    _contentItems() { return `${this.initClass} ` + ( this.config.container || '.smarty-content__item' ) }
+    
+    _activeClass() {
+        return {
+            nav    : this.config.activeController || 'smarty-controller__item--active',
+            content: this.config.activeContent || 'smarty-content__item--active'
         }
-    };
-    return SmartyTabs;
-})();
+    }
+    
+    // helper function
+    static _el( single, collection ) {
+        return collection ? document.querySelectorAll( single ) : document.querySelector( single )
+    }
+    
+    _tabPosition( el ) {
+        let list = SmatryTabs._el( this._navList() );
+        let positionX = el ? ( list.offsetWidth / 2 - el.offsetWidth / 2) - el.offsetLeft : 0;
+        list.style.transform = `translate(${positionX}px)`;
+    }
+    
+    _checkWidth() { return this._tabSlide() && this._maxScreen() <= window.innerWidth }
+    
+    _removeActiveClass() {
+        let itemsCollection = [
+            {
+                items : SmatryTabs._el( this._navItems(), true ),
+                active: this._activeClass().nav
+            },
+            {
+                items : SmatryTabs._el( this._contentItems(), true ),
+                active: this._activeClass().content
+            }
+        ];
+        itemsCollection.forEach( ( list ) => {
+            list.items.forEach( ( item ) => {
+                item.classList.remove( list.active );
+            } )
+        } )
+    }
+    
+    _startStartTab() {
+        SmatryTabs._el( this._navItems(), true )[ this._startIndex() ]
+            .classList.add( this._activeClass().nav );
+        SmatryTabs._el( this._contentItems(), true )[ this._startIndex() ]
+            .classList.add( this._activeClass().content );
+    }
+    
+    _targetTabIndex( el ) {
+        let navList = Array.prototype.slice.call( SmatryTabs._el( this._navItems(), true ) );
+        return navList.indexOf( el );
+    }
+    
+}
+export default function( init, parameters ) {
+    return new SmatryTabs( init, parameters );
+}
